0fa837e2ed71cba33da1287a30def963b513a416
shanimucar <88851097+shanimucar@users.noreply.github.com>
2021-09-03 13:22:07 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    num = int(input())
+    contador = 0
+ 
+    while contador < num:
+        contador = contador + 1
+        if contador % 2 == 0:
+            print(contador, '-#')
+        elif contador % 2 != 0:
+            print(contador, '-%')
+   
 
 if __name__=='__main__':   
     main()
