4562331894a3d018794b287631d172b2eb6c6382
shanimucar <88851097+shanimucar@users.noreply.github.com>
2021-09-03 13:35:11 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,8 +1,16 @@
 
-
 def main():
     #Escribe tu código debajo de esta línea
-    pass
+    num = int(input("Escribe un numero : "))
+    var = 1
+    
+    while var**2 < num:
+        var = var + 1
+        if var**2 < num:
+            pass
+        elif var**2 > num:
+            print(var)
+    
 
 if __name__=='__main__':
     main()
